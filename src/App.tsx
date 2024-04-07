4d41e09a9b704140947bcd56a1e68e5f57f7973a
Ben Lamb <benpaullamb@gmail.com>
2024-04-07 19:17:11 +0100

    refactor: extract search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { getSortedResults, updateResults } from './results-utils';
+import { getSortedResults } from './results-utils';
 import AveragePrice from './AveragePrice';
 import CommonWords from './CommonWords';
+import SearchBar from './SearchBar';
 
 export default () => {
-  const [search, setSearch] = useState('');
   const [results, setResults] = useState<HTMLElement[]>([]);
 
   useEffect(() => {
     setResults(getSortedResults());
   }, []);
 
-  const onSubmit = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key !== 'Enter') return;
-
-    const filteredResults = results.filter(({ title }: HTMLElement) => {
-      const searchRegex = new RegExp((search as string).toLowerCase().trim(), 'ims');
-      return searchRegex.test(title.toLowerCase().trim());
-    });
-
-    updateResults(results, filteredResults);
-  };
-
   return (
     <div className="mb-4 flex flex-col gap-2">
       <AveragePrice results={results} />
       <CommonWords results={results} />
-
-      <input
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        onKeyDown={onSubmit}
-        className="w-full px-2 py-1 border border-[#BBBFBF] rounded"
-        placeholder="Search..."
-      />
+      <SearchBar results={results} />
     </div>
   );
 };
---
+++ src/SearchBar.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { Result, updateResults } from './results-utils';
+
+interface Props {
+  results: Result[];
+}
+
+export default ({ results }: Props) => {
+  const [search, setSearch] = useState('');
+
+  const onSubmit = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key !== 'Enter') return;
+
+    const filteredResults = results.filter(({ title }: Result) => {
+      const searchRegex = new RegExp((search as string).toLowerCase().trim(), 'ims');
+      return searchRegex.test(title.toLowerCase().trim());
+    });
+
+    updateResults(results, filteredResults);
+  };
+
+  return (
+    <input
+      value={search}
+      onChange={(e) => setSearch(e.target.value)}
+      onKeyDown={onSubmit}
+      className="w-1/2 px-2 py-1 border border-[#BBBFBF] rounded"
+      placeholder="Search..."
+    />
+  );
+};
